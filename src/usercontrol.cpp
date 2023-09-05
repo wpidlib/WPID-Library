63b042ac3538e1f84ed8d6154f4282eeff7428e3
JaiiR320 <jair3202@gmail.com>
2023-09-05 17:16:59 -0400

    Update usercontrol.cpp

--- src/usercontrol.cpp
+++ src/usercontrol.cpp
@@ -1,17 +1,17 @@
 #include "main.h"
 
-// square a value
-double sqr(double x) { return x * x; }
-// returns a % of 127 * 100
-double scale(double x) { return 100 * (sqr(x)/127); }
+// cubes value and returns as a proportion
+double scale(double x) {
+    return (x > 0 ? 100 : -100) * ((x/127) * (x/127)); 
+}
 
 void usercontrol(){
     double axis3, axis4 = 0;
     while (1) {
         axis3 = scale(con->Axis3.value());
         axis4 = scale(con->Axis4.value());
-        chassis->spin(axis3 + axis4, axis3 - axis4); 
+        chassis->spin(axis3 - axis4, axis3 + axis4); 
         
-        delay(20);
+        delay(20); 
     }
 }
